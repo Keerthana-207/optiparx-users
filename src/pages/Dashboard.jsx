6296d8026032356ca669a66b258258ecef74ef6e
Example Name <name@example.com>
2025-10-17 00:13:34 +0530

    Initial commit

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,17 @@
 function Dashboard({ userData, onLogout, onNavigate }) {
+  const update = async () => {
+    const res = await (fetch('https://optiparx-users.netlify.app/api/slots/device-message'));
+    return res.json()
+    
+  }
   return (
     <div className="container">
       <div className="step active">
         <img src="logo.png" alt="OptiParX Logo" className="logo" />
         <h2>Welcome, {userData?.username || 'User'}</h2>
+        <div>
+          {update}
+        </div>
         <div className="btn-container">
           <button onClick={() => onNavigate('book')}>Book a Slot</button>
           <button onClick={() => onNavigate('history')}>View Booking History</button>
