03896b1d6622e479143ecd37f869a96bdd96b632
Keerthana <keert_207@yahoo.com>
2025-10-17 12:06:17 +0530

    Update FinalPayment.jsx

--- src/components/FinalPayment.jsx
+++ src/components/FinalPayment.jsx
@@ -35,7 +35,7 @@ function FinalPayment({ userData, onFinish }) {
     };
 
     try {
-      const res = await fetch("https://optiparx-backend-9gc0lmpqj-keerthana-207s-projects.vercel.app/book", {
+      const res = await fetch("https://optiparx-backend.onrender.com/book", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
